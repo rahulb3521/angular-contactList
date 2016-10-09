3d232db65004efe5700b785de825ebf0ba265720
Raghav Bhalla <Raghav Bhalla>
2016-10-09 21:35:09 +0530

    #new str

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/app'));
 app.listen(3000);
-console.log('Server running on port 3000 ');
+console.log('Server running on port 3000 '); 
